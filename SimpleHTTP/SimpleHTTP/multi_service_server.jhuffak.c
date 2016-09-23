f67dc26b8841fcbf74e45456410b48f98913e897
Jordan Huffaker <jhuffak@hci.ecn.purdue.edu>
2016-09-22 20:14:03 -0400

    finished the code i think, time to test!

--- SimpleHTTP/SimpleHTTP/multi_service_server.jhuffak.c
+++ SimpleHTTP/SimpleHTTP/multi_service_server.jhuffak.c
@@ -33,30 +33,57 @@ int main(int argc, char **argv)
 	//get_page(test);
 	//close(test);
 
-	int listenfd, connfd, port, clientlen;
+	int listenfd, listenfd2, connfd, port, port2, clientlen, maxfd, retval;
+	uint32_t ping;
 	struct sockaddr_in clientaddr;
 	struct hostent *hp;
 	char *haddrp;
-	port = atoi(argv[1]); // the server listens on a port passed on the command line
+	struct fd_set rfds;
+	char buf[MAXLINE];
+	port = atoi(argv[1]); // the http server listens on a port passed on the command line
+	port2 = atoi(argv[2]); // the udp server listens on a port passed on the command line
 	pid_t childpid;
 	signal(SIGCHLD, SIG_IGN);
 	listenfd = open_listenfd(port);
+	listenfd2 = open_listenfd_udp(port2);
+
 	while (1)
 	{
-		clientlen = sizeof(clientaddr);
-		connfd = accept(listenfd, (struct sockaddr *) &clientaddr, &clientlen);
-		if ((childpid = fork()) == 0)
+		FD_ZERO(&rfds);
+		FD_SET(listenfd);
+		FD_SET(listenfd2);
+		maxfd = max(listenfd, listenfd2) + 1;
+		retval = select(maxfd, &rfds, NULL, NULL, NULL);
+		if (FD_ISSET(listenfd, &rdfs))
 		{
-			close(listenfd);
-			hp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-			sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-			haddrp = inet_ntoa(clientaddr.sin_addr);
-			//printf("Fd %d connected to %s (%s:%s)\n", connfd, hp->h_name, haddrp, ntohs(clientaddr.sin_port));
-			get_page(connfd);
+			clientlen = sizeof(clientaddr);
+			connfd = accept(listenfd, (struct sockaddr *) &clientaddr, &clientlen);
+			if ((childpid = fork()) == 0)
+			{
+				close(listenfd);
+				hp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+				sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+				haddrp = inet_ntoa(clientaddr.sin_addr);
+				//printf("Fd %d connected to %s (%s:%s)\n", connfd, hp->h_name, haddrp, ntohs(clientaddr.sin_port));
+				get_page(connfd);
+				close(connfd);
+				exit(0);
+			}
 			close(connfd);
-			exit(0);
 		}
-		close(connfd);
+		else if (FD_ISSET(listenfd2, &rfds))
+		{
+			clientlen = sizeof(clientaddr);
+			recvfrom(listenfd2, buf, 12, 0, (struct sockaddr *) &clientaddr, &clientlen);
+
+			ping = memcpy(&ping, buf, sizeof(uint32_t));
+			ping = ntohl(ping) + 1;
+			ping = htonl(ping);
+			memcpy(buf, &ping, sizeof(uint32_t));
+
+			sendto(listenfd2, buf, 12, 0, (struct sockaddr *) &clientaddr, clientlen);
+			//close(listenfd2);
+		}
 	}
 }
 
@@ -117,6 +144,31 @@ void echo(int connfd)
 		printf("server received %d bytes\n", n);
 		write(connfd, buf, n);
 	}
+}
+
+int open_listenfd_udp(int port)
+{
+	int listenfd, optval=1;
+	struct sockaddr_in serveraddr;
+
+	/* Create a socket descriptor */
+	if ((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		return -1;
+
+	/* Eliminates "Address already in use" error from bind. */
+	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+		(const void *)&optval , sizeof(int)) < 0)
+		return -1;
+	/* Listenfd will be an endpoint for all requests to port
+	on any IP address for this host */
+	bzero((char *) &serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)port);
+	if (bind(listenfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
+		return -1;
+	
+	return listenfd;
 }
 
 // CREDIT: Sanjay Rao - Lecture Socket Programming slide 24-25
